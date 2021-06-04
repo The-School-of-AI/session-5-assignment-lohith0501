e16eef2c49ff78a526f211b185ddc8ccf07d8279
lohith0501 <lohith_gn@yahoo.com>
2021-06-04 17:24:54 +0530

    Session_5_assgn_commit_1

--- README.md
+++ README.md
@@ -1,1 +1,88 @@
-# epai3session5
+## Assignment 5 on Functional Parameters
+- Submission by Lohith G N (EPAI batch 3)
+
+### Summary of assignment
+- This is an assignment where we are expected to write a time it function which is descrived below
+- Write a function that gives out the average run time per call, such that its definition is:
+- def time_it(fn, *args, repetitons= 1, **kwargs): your code comes here.
+
+- We should be able to call it like this:
+
+- time_it(print, 1, 2, 3, sep='-', end= ' ***\n'. repetitons=5)
+- time_it(squared_power_list, 2, start=0, end=5, repetitons=5) #2 is the number you are calculating power of, [1, 2, 4, 8, 16, 32]
+- time_it(polygon_area, 15, sides = 3, repetitons=10) # 15 is the side length. This polygon supports area calculations of upto a hexagon
+- time_it(temp_converter, 100, temp_given_in = 'f', repetitons=100) # 100 is the base temperature given to be converted
+- time_it(speed_converter, 100, dist='km', time='min', repetitons=200) #dist can be km/m/ft/yrd, time can be ms/s/m/hr/day, speed given by user is in kmph
+
+### Functions in session 5 main module
+There are six functions in this module as explained below. custom_print is the shortest which just prints the parameters that are passed to it from test module.
+
+- time_it(fn, *args, repetitions=5, **kwargs)
+    - This is a function which takes all positional and kyword arguments using args and kwargs concept. There is 1 defined positional argument named fn and keyword argument named repitations with default value set to 5. fn takes function as argyment and is a genralized function to call any function user specified number of times and return the average time taken for calls
+
+- squared_power_list(number, *args, start=0, end=5, **kwargs)
+	- This function retruns list by raising number to power from start to end
+	- number**start to number**end. Default start is 0 and end is 5
+    - There is 1 positional argument named number and keyword argument named start and end. rest are taken care by args and kwargs.
+
+- polygon_area(length, *args, sides=3, **kwargs)
+	- This function retruns area of a regular polygon with number of sides between
+    3 to 6 bith inclusive
+    - Here length needs to be greater than 0 and sides need to be between 3 and 6.
+
+- temp_converter(temp, *args, temp_given_in='F', **kwargs)
+	- This function converts temprature from celsius 'C' to fahrenheit 'F' or
+    fahrenheit to celsius
+
+- speed_converter(speed, *args, dist='KM', time='MIN', **kwargs)
+	- This function converts speed from kmph (provided by user as input) to different units
+    dist can be km/m/ft/yrd time can be ms/s/min/hr/day
+
+- __eq__(self, value)
+	- Checks if value of the qualean object is equal to value
+	- value can be int, float, bool or decimal
+
+### Test cases 
+- Readme contents check for is a list of keywords which will be checked in readme file if it is present or not. Below is the list for reference.
+    - README_CONTENT_CHECK_FOR = [
+    'time_it(fn, *args, repetitions= 1, **kwargs)',
+    'squared_power_list',
+    'polygon_area',
+    'temp_converter',
+    'speed_converter'
+]
+
+- test_session5_readme_exists()
+    - Thuis will check if readme file exists along with other modules.
+
+- test_session5_readme_500_words()
+    - This will check if contents in readme file are having greater than 500 words.
+
+- test_session5_readme_proper_description()
+    - This will check if README_CONTENT_CHECK_FOR content of words are present in readme file or not.
+	
+- test_session5_readme_file_for_more_than_10_hashes
+    - This will check formating of session5.py. It will check for atleast 10 hashes are present in readme file.
+
+- test_session5_indentations()
+    - This will check if Your script contains misplaced indentations.
+    - Also check code indentation whether it follows PEP8 guidelines
+
+- test_session5_function_name_had_cap_letter()
+    - This test will check if you have used Capital letter(s) in your function names
+
+- test_session5_time_it_print()
+    - This function will print whatever is passed through here to main module.
+
+- test_function_qualean_type
+    - This will check if the input number is of Qualean class type.
+
+- Some of the other test cases are pertaining to
+    - Validations for time_it
+    - Validations for squared_power_list
+    - Validations for polygon_area
+    - Validations for temp_converter
+    - Validations for speed_converter
+
+Thank You
+
--- session5.py
+++ session5.py
@@ -1,52 +1,186 @@
 """WRITE PROPER ASSINGMENT DESCIPTION HERE AND DELETE THIS MESSAGE"""
 
 import time
+import math
+import timeit
 
-def time_it(fn, *args, repetitions= 1, **kwargs):
+from attr import validate
+
+
+def time_it(fn, *args, repetitions=5, **kwargs):
     """This is a genralized function to call any function
     user specified number of times and return the average
     time taken for calls"""
 
     # Repetition should be positive number
-    pass
+    if repetitions == 0:
+        return 0
+    else:
+        start = timeit.timeit()
+        for _ in range(0, repetitions):
+            value = fn(*args, **kwargs)
+        end = timeit.timeit()
+        print("Total Time:", end - start)
+        speed = end - start
+        return value, speed
 
-def squared_power_list(number,*args, start=0, end=5,**kwargs):
+
+def custom_print(*args, **kwargs):
+    print(*args, **kwargs)
+
+
+def squared_power_list(number, *args, start=0, end=5, **kwargs):
     """Retruns list by raising number to power from start to end
     -> number**start to number**end. Default start is 0 and end is 5"""
 
     # Validations "if" block
-    
 
     # Return the list of number to the power of numbers from start to end
+    if start < 0 or end < 0:
+        raise ValueError("Value of start or end can't be negative")
 
-    pass
+    if start > end:
+        raise ValueError("Value of start should be less than end")
 
-def polygon_area(length, *args, sides = 3, **kwargs):
+    if isinstance(number, str) or isinstance(number, complex):
+        raise TypeError("Only integer type arguments are allowed")
+
+    if number > 10:
+        raise ValueError("Value of number should be less than 10")
+
+    if args:
+        raise TypeError("takes maximum 1 positional arguments")
+
+    if kwargs:
+        raise TypeError("maximum 2 keyword/named arguments")
+
+    else:
+        s_list = []
+        for i in range(start, end):
+            s_list.append(pow(number, i))
+    return s_list
+
+
+def polygon_area(length, *args, sides=3, **kwargs):
     """Retruns area of a regular polygon with number of sides between
     3 to 6 bith inclusive"""
 
     # Validations
+    if isinstance(length, str) or isinstance(length, complex):
+        raise TypeError("Only integer type arguments are allowed")
+
+    if isinstance(sides, str) or isinstance(sides, complex):
+        raise TypeError("Only integer type arguments are allowed")
+
+    if args:
+        raise TypeError(
+            "polygon_area function takes maximum 1 positional arguments, more provided")
+
+    if kwargs:
+        raise TypeError(
+            "polygon_area function take maximum 1 keyword/named arguments, more provided")
 
     # Return area
-    pass
+    if (sides >= 3 and sides <= 6) and (length > 0):
+        area = sides * (length ** 2) / (4 * math.tan(math.pi / sides))
+        return area
+    else:
+        raise ValueError(
+            "Sides haves to be greater than 2 or less than 7 and length shouldnt be negative or zero, it should be positive")
 
-def temp_converter(temp, *args, temp_given_in = 'f', **kwargs):
+
+def temp_converter(temp, *args, temp_given_in='F', **kwargs):
     """Converts temprature from celsius 'c' to fahrenheit 'f' or
     fahrenheit to celsius"""
 
     # Validations
+    if isinstance(temp, str) or isinstance(temp, complex):
+        raise TypeError("Only integer type arguments are allowed")
+
+    if isinstance(temp_given_in, str) and temp_given_in not in ['F', 'C']:
+        raise ValueError("Only f or c is allowed")
+
+    if isinstance(temp_given_in, complex):
+        raise TypeError("Charcater string expected")
+
+    if args:
+        raise TypeError(
+            "temp_converter function takes maximum 1 positional arguments, more provided")
+
+    if kwargs:
+        raise TypeError(
+            "temp_converter function take maximum 1 keyword/named arguments, more provided")
 
     # Return the converted temprature
-    
-    pass
 
-def speed_converter(speed, *args, dist='km', time='min', **kwargs):
+    if temp_given_in == 'F':
+        if temp > -459.67:
+            celcius_temp = ((temp - 32)*5)/9
+            return celcius_temp
+        else:
+            raise ValueError(
+                "Temprature can't go below -459.67 fahrenheit = 0 Kelvin")
+    elif temp_given_in == 'C':
+        if temp > -273.15:
+            fahrenheit_temp = (temp*9)/5 + 32
+            return fahrenheit_temp
+        else:
+            raise ValueError(
+                "Temprature can't go below -273.15 celsius = 0 Kelvin")
+
+
+def speed_converter(speed, *args, dist='KM', time='MIN', **kwargs):
     """Converts speed from kmph (provided by user as input) to different units
     dist can be km/m/ft/yrd time can be ms/s/min/hr/day """
 
     # Validations
-    
-
+    if isinstance(speed, str) or isinstance(speed, complex):
+        raise TypeError("Speed can be int or float type only")
     # Return the converted speed
-    
-    pass
+
+    if isinstance(dist, int) or isinstance(dist, complex):
+        raise TypeError("Charcater string expected for distance unit")
+
+    if isinstance(time, int) or isinstance(time, complex):
+        raise TypeError("Charcater string expected")
+
+    if speed > 300000:
+        raise ValueError("Speed can't be greater than speed of light")
+
+    if isinstance(time, str) and time not in ['MS', 'S', 'MIN', 'HR', 'DAY']:
+        raise ValueError(
+            "Incorrect unit of Time. Only ms/s/min/hr/day allowed")
+
+    if isinstance(dist, str) and dist not in ['KM', 'M', 'FT', 'YRD']:
+        raise ValueError(
+            "Incorrect unit of distance. Only km/m/ft/yrd allowed")
+
+    if args:
+        raise TypeError(
+            "speed_converter function takes maximum 1 positional arguments, more provided")
+    if kwargs:
+        raise TypeError(
+            "speed_converter function take maximum 2 keyword/named arguments, more provided")
+
+    if speed > 0:
+        if dist == 'KM':
+            speed = speed
+        elif dist == 'M':
+            speed = speed * 1000
+        elif dist == 'FT':
+            speed = speed * 3280.8375
+        elif dist == 'YRD':
+            speed = speed * 1093.609
+        if time == 'HR':
+            speed = speed
+        elif time == 'DAY':
+            speed = speed * 24
+        elif time == 'MIN':
+            speed = speed / 60
+        elif time == 'S':
+            speed = speed / (60 * 60)
+        elif time == 'MS':
+            speed = speed / 3600000
+        return round(speed)
+    else:
+        raise ValueError("Speed can't be negative")
